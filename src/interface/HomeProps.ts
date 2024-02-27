ef5af25d314699f228ec9127aa0d706828c83346
unknown <mcbelme92@gmail.com>
2024-02-26 20:22:11 -0600

    Implement_finaldev_Doc_HomeProps

--- src/interface/HomeProps.ts
+++ src/interface/HomeProps.ts
@@ -1,3 +1,9 @@
+/**
+ * Interfaz que representa las props para el componente Home.
+ * @interface HomeProps
+ * @property {string} title - El título que se mostrará en el componente Home.
+ */
+
 export interface HomeProps {
   title: string;
 }
