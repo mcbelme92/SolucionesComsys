4a539e7dd6ddbf74a9b1d81bb2877730e3e0e3f0
unknown <mcbelme92@gmail.com>
2024-03-03 19:24:21 -0600

    Implement_indexhtml_metadescription_witouth_useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,8 @@ import Home from "./components/Home/Home";
 //Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const metaDescription = document.createElement("meta");
-    metaDescription.name = "description";
-    metaDescription.content = "Da publicaciones estrucutradas por usuario";
-    document.head.appendChild(metaDescription);
-
-    // Limpieza al desmontar el componente
-    return () => {
-      document.head.removeChild(metaDescription);
-    };
-  }, []);
-
   return (
     <div>
       <Home title="Soluciones Comsys" />
